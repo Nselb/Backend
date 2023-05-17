7f21d1e5fea2a23023b8e964e379620d38df45a2
Sahid Carchi <sahidsebastian@hotmail.com>
2023-05-16 21:47:34 -0500

    a

---
+++ Backend/Controllers/AutorizadorController.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Models;
+using BackendAja.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorizadorController : ControllerBase
+    {
+        // GET: api/<AutorizadorController>
+        [HttpGet("{id}/{contrasena}")]
+        public async Task<Autorizador?> Get(int id, string contrasena)
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/");
+
+            var responseTask = client.GetAsync($"GetAutorizador?usuario={id}&password={contrasena}");
+            var result = responseTask.Result;
+            Autorizador? usuario = null;
+            if (result.IsSuccessStatusCode)
+            {
+                List<Autorizador?>? coso = await result.Content.ReadFromJsonAsync<List<Autorizador?>>();
+                usuario = coso?.FirstOrDefault();
+            }
+            return usuario;
+        }
+    }
+}
---
+++ Backend/Models/Autorizador.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Models
+{
+    public class Autorizador
+    {
+        public string NOMBREUSUARIO { get; set; }
+        public string PERFIL { get; set; }
+        public string OBSERVACION { get; set; }
+        public string CODIGOPERFIL { get; set; }
+        public string ESTADO { get; set; }
+        public string COMPANIA { get; set; }
+        public string Emisor { get; set; }
+        public string Cargo { get; set; }
+        public string NOMBREEMISOR { get; set; }
+        public string NOMBRECOMPANIA { get; set; }
+        public string USUARIOCLIENTE { get; set; }
+        public string RucUsuario { get; set; }
+    }
+
+}
